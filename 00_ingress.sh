fd81dc025074fcd67e652fa85de0d19699c6c293
rovandep <romuald.vandepoel@storageos.com>
2021-06-24 12:36:17 +0200

    add: demo scripts

--- 00_ingress.sh
+++ 00_ingress.sh
@@ -4,6 +4,11 @@ echo "--> install nginx-ingress"
 helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
 echo "--> get ingress LB cluster IP"
 kubectl get services -A -w |grep ingress
+echo "--> install cert-manager"
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
 echo "--> deploy certmanager"
 kubectl apply -f gke_certmanager_issuer.yaml 
 
