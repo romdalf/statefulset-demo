4702bfa3d217db8cdc777488355c74a66555be74
rovandep <romuald.vandepoel@storageos.com>
2021-06-24 12:10:21 +0200

    add: demo scripts

--- 03_storageClass.sh
+++ 03_storageClass.sh
@@ -3,9 +3,9 @@ echo "kubectl get nodes"
 kubectl get storageclass 
 echo "" 
 echo "--> deploy storageOS storageClass / 0 replica / encryption off"
-echo "kubectl apply -f storageClass_basic.yml"
+echo "kubectl apply -f storageClass_basic.yaml"
 kubectl apply -f storageClass_basic.yml
 echo "" 
 echo "--> deploy storageOS storageClass / 1 replica / encryption on"
-echo "kubectl apply -f storageClass_basic.yml"
+echo "kubectl apply -f storageClass_basic.yaml"
 kubectl apply -f storageClass_rep1_encryption.yml
