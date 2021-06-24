30384d2980e29d52b1b4ff058785ea9e229d3d78
rovandep <romuald.vandepoel@storageos.com>
2021-06-24 12:28:34 +0200

    add: demo scripts

--- 00_ingress.sh
+++ 00_ingress.sh
@@ -1,3 +1,9 @@
+echo "--> add helm repo for ingress-nginx"
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+echo "--> install nginx-ingress"
 helm install nginx-ingress ingress-nginx/ingress-nginx --set controller.publishService.enabled=true
-k get services -A |grep ingress
+echo "--> get ingress LB cluster IP"
+kubectl get services -A -w |grep ingress
+echo "--> deploy certmanager"
+kubectl apply -f gke_certmanager_issuer.yaml 
+
