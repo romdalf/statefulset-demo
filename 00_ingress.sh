ed8b8c12d48dbca2ef91e4fb229a34d87c9e32ae
Rom;) <55788733+rovandep@users.noreply.github.com>
2021-07-16 12:14:04 +0200

    Update 00_ingress.sh

--- 00_ingress.sh
+++ 00_ingress.sh
@@ -11,6 +11,8 @@ kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
+echo "--> wait for cert-manager pods"
+kubectl get pod -n cert-manager -w
 echo "--> deploy certmanager"
 kubectl apply -f gke_certmanager_issuer.yaml 
 echo "" 
