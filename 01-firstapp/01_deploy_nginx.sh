f6e95c8dbce1398aea9c1f89ca954cec0e007ad6
rovandep <romuald.vandepoel@storageos.com>
2021-09-29 14:48:51 +0200

    refactoring deploy

--- 01-firstapp/01_deploy_nginx.sh
+++ 01-firstapp/01_deploy_nginx.sh
@@ -1,24 +1,31 @@
-echo "Deploying our first application!"
+
+# Define some colours for later
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+echo -e "Deploying our first application!"
 echo ""
-echo "--> Sending the yaml definition to Kubernetes..."
-echo "kubectl apply -f nginx.yaml"
+echo -e "--> Sending the yaml definition to Kubernetes..."
+echo -e "${BLUE}kubectl apply -f nginx.yaml${NC}"
 kubectl apply -f nginx.yaml
 echo "" 
 sleep 10
-echo "--> Checking the status of the statefulset"
-echo "kubectl get all -n web"
+echo -e "--> Checking the status of the statefulset"
+echo -e "${BLUE}kubectl get all -n web${NC}"
 kubectl get all -n web
 echo ""
-echo "--> Checking the creation of the PersistentVolumeClaim"
-echo "kubectl get pvc -A"
+echo -e "--> Checking the creation of the PersistentVolumeClaim"
+echo -e "${BLUE}kubectl get pvc -A${NC}"
 kubectl get pvc -n web
 echo "" 
-echo "--> Checking the creation of the PersistentVolume"
-echo "kubectl get pv"
+echo -e "--> Checking the creation of the PersistentVolume"
+echo -e "${BLUE}kubectl get pv${NC}"
 kubectl get pv
 echo ""
-echo "--> Check the PersistentVolume from a StorageOS view"
-echo "kubectl get pv"
+echo -e "--> Check the PersistentVolume from a StorageOS view"
+echo -e "${BLUE}kubectl get pv${NC}"
 kubectl exec -n kube-system -it cli -- storageos get volumes -n web
 echo ""
-echo "Simple like A, B, C!"
+echo -e "${BLUE}Simple like A, B, C!${NC}"
